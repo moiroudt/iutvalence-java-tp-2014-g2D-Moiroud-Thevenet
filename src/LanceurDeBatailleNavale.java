53b4688e024ab3116f45e328702e4a3198bd2f2a
moiroudt <thibaut.moiroud@iut-valence.fr>
2014-02-17 10:38:01 +0100

    on a ajouté une méthode à cette classe qui crée une nouvelle partie puis la lance.

---
+++ src/LanceurDeBatailleNavale.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author moiroudt
+ * Lance une partie de bataille navale
+ */
+public class LanceurDeBatailleNavale {
+
+	/**
+	 * Création d'une partie de bataille navale
+	 * Après la création de cette partie on la joue.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BN partie = new BN[];
+		paritie.jouer();
+		
+	}
+
+}
