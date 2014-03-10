2a8c80d9346f1500aeb6dd0563cc81aad5e7a407
moiroudt <thibaut.moiroud@iut-valence.fr>
2014-03-10 11:24:08 +0100

    création de la classe Position.

---
+++ src/jeudebataillenavale/Position.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package jeudebataillenavale;
+
+/**
+ * @author moiroudt
+ *
+ */
+public class Position
+{
+	private int x; //x definit l'abscisse de la position de la proue
+	private int y; //y definit l'ordonnee de la position de la proue
+	
+	/** On créer l'objet position */
+	public Position(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+}
