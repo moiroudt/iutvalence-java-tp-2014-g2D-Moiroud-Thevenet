b792ae06fb84587684e0622b5c5cfa33f07132a6
moiroudt <thibaut.moiroud@iut-valence.fr>
2014-03-21 14:06:21 +0100

    Ajout de commentaires + changement de méthode (même si non terminée)

---
+++ src/fr/iutvalence/java/tp/bataillenavale/Bateau.java
@@ -0,0 +1,24 @@
+package jeudebataillenavale;
+
+/**
+ * 
+ * 
+ * @author moiroudt
+ *
+ */
+public class Bateau //definition de la pièce Bateau.
+{
+	private int taille; //taille definit le nombre de cases qu'occupe un bateau
+	private boolean estHorizontal; //horizontal definit la direction d'un bateau
+	private Position positionProue;
+	
+	/** On creer l'objet bateau */
+	public Bateau (int x, int y, boolean estHorizontal, int taille)
+	{
+		positionProue = new Position
+		if (horizontal == true)
+		{
+			
+		}
+	}
+}
