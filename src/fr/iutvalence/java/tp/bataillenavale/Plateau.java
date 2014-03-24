177fe6823529597770f2bde13566315802bbc81f
moiroudt <thibaut.moiroud@iut-valence.fr>
2014-03-24 12:20:49 +0100

    création complète de la classe Plateau

--- src/fr/iutvalence/java/tp/bataillenavale/Plateau.java
+++ src/fr/iutvalence/java/tp/bataillenavale/Plateau.java
@@ -13,12 +13,12 @@ public class Plateau
 	/**
 	 * nombre_de_cases_en_abscisse definit le nombre de cases horizontales
 	 */
-	private final int nombre_de_cases_en_abscisse = 10;
+	private static final int nombre_de_cases_en_abscisse = 10;
 	
 	/**
 	 * nombre_de_cases_en_ordonnee definit le nombre de cases verticales
 	 */
-	private final int nombre_de_cases_en_ordonnee = 10;
+	private static final int nombre_de_cases_en_ordonnee = 10;
 	
 	/**
 	 * les cases du plateau
@@ -33,4 +33,25 @@ public class Plateau
 		this.cases = new Case[nombre_de_cases_en_abscisse][nombre_de_cases_en_ordonnee];
 		
 	}
+	
+	/**
+	 * Obtenir la case de l'echiquier a une position donnee
+	 * @param position la position
+	 * @return la case du plateau a la postion donnee
+	 */
+	private Case obtenirCase(Position position)
+	{
+		return this.cases[position.getx()][position.gety()];
+	}
+	
+	/**
+	 * Obtenir le bateau pose sur une case de position donee
+	 * @param positionProue la postion de la proue du bateau
+	 * @return le bateau pose sur la case
+	 */
+	public Bateau obtenirBateau(Position positionProue)
+	{
+		return this.cases[positionProue.getx()][positionProue.gety()].obtenirBateau();
+	}
+	
 }
