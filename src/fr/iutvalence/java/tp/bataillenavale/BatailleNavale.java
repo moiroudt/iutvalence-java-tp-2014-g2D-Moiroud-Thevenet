2d28719e5ad61caea7c460d29986af8265f17d53
moiroudt <thibaut.moiroud@iut-valence.fr>
2014-04-03 10:50:57 +0200

    création et documentation de la classe BatailleNavale

--- src/fr/iutvalence/java/tp/bataillenavale/BatailleNavale.java
+++ src/fr/iutvalence/java/tp/bataillenavale/BatailleNavale.java
@@ -10,5 +10,42 @@ package fr.iutvalence.java.tp.bataillenavale;
  */
 public class BatailleNavale
 {
-
+	/**
+	 * les 2 joueurs d'une partie de bataille navale
+	 */
+	private Joueur joueur1;
+	private Joueur joueur2;
+	
+	/**
+	 * le plateau ou sont positionnes les bteaux de chaque joueur
+	 */
+	private Bateau[] bateauxJoueur1;
+	private Bateau[] bateauxJoueur2;
+	
+	/**
+	 * le plateau qui indique ou les joueurs ont tire
+	 */
+	private boolean[][] tirsJoueur1;
+	private boolean[][] tirsJoueur2;
+	
+	/**
+	 * @param bateauxJoueur1
+	 * @param bateauxJoueur2
+	 */
+	public BatailleNavale(Bateau[] bateauxJoueur1, Bateau[] bateauxJoueur2)
+	{
+		super();
+		this.bateauxJoueur1 = bateauxJoueur1;
+		this.bateauxJoueur2 = bateauxJoueur2;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public void jouer()
+	{
+		
+	}
+	
 }
